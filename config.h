79a21fecd1cec4a90f0969ea6a76f39e799a3ad9
guh123 <1803350493@qq.com>
2020-09-28 09:52:39 +0800

    rofi

--- config.h
+++ config.h
@@ -78,6 +78,7 @@ static const char *downvol[] = { "/home/dyear/scripts/vol-down.sh",  NULL };
 static const char *mutevol[] = { "/home/dyear/scripts/vol-toggle.sh",  NULL };
 static const char *burpcmd[]= {"/home/dyear/scripts/burp.sh",NULL};
 static const char *rangercmd[]= {"ranger",NULL};
+static const char *rofi[] = {"rofi","-combi-modi","drun,run","-show","modi"};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -86,7 +87,7 @@ static Key keys[] = {
 	{ 0,                   XF86XK_AudioMute,        spawn,          {.v = mutevol } },
 	{ 0,                   XF86XK_AudioRaiseVolume, spawn,          {.v = upvol   } },
 	{ MODKEY|ShiftMask,              XK_t,      spawn,          {.v = trayercmd }},
-	{ MODKEY|ShiftMask,              XK_r,      spawn,          {.v = rangercmd }},
+	{ MODKEY,              XK_r,      spawn,          {.v = rofi }},
 	{ MODKEY,                       XK_p,      spawn,    	   {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
